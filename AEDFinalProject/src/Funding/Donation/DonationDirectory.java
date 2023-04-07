c8c9518da69e9d2ed4b94fdada77d9d891f589af
srushti-ghatage <ghatage.s@northeastern.edu>
2023-04-07 18:49:35 -0400

    funding added

---
+++ AEDFinalProject/src/Funding/Donation/DonationDirectory.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Funding.Donation;
+
+/**
+ *
+ * @author 18573
+ */
+public class DonationDirectory {
+    
+}
