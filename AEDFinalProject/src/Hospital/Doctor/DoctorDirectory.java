834cb295cc4d9466d92b573d54ff0c237e49eea3
srushti-ghatage <ghatage.s@northeastern.edu>
2023-04-07 14:15:38 -0400

    added doctors directory

--- AEDFinalProject/src/Hospital/Doctor/DoctorDirectory.java
+++ AEDFinalProject/src/Hospital/Doctor/DoctorDirectory.java
@@ -4,10 +4,35 @@
  */
 package Hospital.Doctor;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author SrushtiGhatage
  */
 public class DoctorDirectory {
+      
+    private ArrayList<Doctor> doctorList;
     
+    public DoctorDirectory() {
+        doctorList = new ArrayList<Doctor>();   
+    }
+
+    public ArrayList<Doctor> getDoctorList() {
+        return doctorList;
+    }
+
+    public void setDoctorList(ArrayList<Doctor> doctorList) {
+        this.doctorList = doctorList;
+    }
+    
+    public Doctor addDoctor(){
+        Doctor newdoctor = new Doctor();
+        doctorList.add(newdoctor);
+        return newdoctor;
+    }
+    
+    public void deleteDoctor(Doctor m){
+        doctorList.remove(m);
+    }
 }
