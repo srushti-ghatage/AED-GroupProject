1f8c539ca84aba0cb220f953b031c5bc52f929a4
srushti-ghatage <ghatage.s@northeastern.edu>
2023-04-08 19:09:02 -0400

    updated data validation

--- AEDFinalProject/src/System/Directories/DataValidation.java
+++ AEDFinalProject/src/System/Directories/DataValidation.java
@@ -16,4 +16,28 @@ public class DataValidation {
         }
         return true;
     }
+      public boolean numcheck(String val){
+        try{
+            int temp = Integer.parseInt(val);
+            if(Integer.valueOf(val)<0){
+                return false;
+            }
+        }catch(Exception err){
+            return false;
+        }
+        return true;
+    }
+     
+     public boolean phnocheck(String val){
+        try{
+            long temp = Long.parseLong(val);
+        }catch(Exception err){
+            return false;
+        }
+        
+        if(val.length()!=10){
+            return false;
+        }
+        return true;
+    }
 }
