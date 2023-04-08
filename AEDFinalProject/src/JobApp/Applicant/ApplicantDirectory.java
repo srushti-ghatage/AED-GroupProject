8446dbccffbec377fa81508aaf03e684185593ad
srushti-ghatage <ghatage.s@northeastern.edu>
2023-04-08 14:15:30 -0400

    updated applicant

--- AEDFinalProject/src/JobApp/Applicant/ApplicantDirectory.java
+++ AEDFinalProject/src/JobApp/Applicant/ApplicantDirectory.java
@@ -13,6 +13,50 @@ import java.util.ArrayList;
  */
 public class ApplicantDirectory {
     
+     private ArrayList<Applicant> appList;
+
+    public ApplicantDirectory() {
+        appList = new ArrayList<Applicant>();
+        
+    }
+
+    public ArrayList<Applicant> getAppList() {
+        return appList;
+    }
+
+    public void setAppList(ArrayList<Applicant> appList) {
+        this.appList = appList;
+    }
     
+    public Applicant addUser(){
+        Applicant p = new Applicant();
+        appList.add(p);
+        return p;
+    }
+    
+    public void deleteUser(Applicant p){
+        appList.remove(p);
+    }
+    
+     public Applicant findApplicant(String username, String password){
+        for(Applicant p: appList){
+            if(p.getUsername().equals(username) && p.getPassword().equals(password)){
+                return p;
+            }
+            
+        }
+        return null;
+    }
+
+     public boolean uiqueApplicantUsername(String id) {
+
+        for (Applicant d : appList) {
+
+            if (id.equals(d.getUsername())) {
+                return false;
+            }
+        }
+            return true; 
+    } 
             
 }
