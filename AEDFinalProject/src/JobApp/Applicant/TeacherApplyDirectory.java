4be5c50da74034d5a66767ad48c873037c1d57e1
srushti-ghatage <ghatage.s@northeastern.edu>
2023-04-08 15:30:02 -0400

    added teacher apply

--- AEDFinalProject/src/JobApp/Applicant/TeacherApplyDirectory.java
+++ AEDFinalProject/src/JobApp/Applicant/TeacherApplyDirectory.java
@@ -4,10 +4,46 @@
  */
 package JobApp.Applicant;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author SrushtiGhatage
  */
 public class TeacherApplyDirectory {
+      private ArrayList<TeacherApply> appList;
+
+    public TeacherApplyDirectory() {
+        appList = new ArrayList<TeacherApply>();
+        
+    }
+
+    public ArrayList<TeacherApply> getAppList() {
+        return appList;
+    }
+
+    public void setAppList(ArrayList<TeacherApply> appList) {
+        this.appList = appList;
+    }
     
+    public TeacherApply addApp(){
+        TeacherApply p = new TeacherApply();
+        appList.add(p);
+        return p;
+    }
+    
+    public void deleteApp(TeacherApply p){
+        appList.remove(p);
+    }
+    
+    public boolean uiqueJobAppId(String id) {
+
+        for (TeacherApply d : appList) {
+
+            if (id.equals(d.getAppId())) {
+                return false;
+            }
+        }
+            return true; 
+    } 
 }
