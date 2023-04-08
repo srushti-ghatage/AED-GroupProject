60931c3663ee55da5809607caa135c0bb10ef41b
srushti-ghatage <ghatage.s@northeastern.edu>
2023-04-08 13:09:59 -0400

    added applicant

---
+++ AEDFinalProject/src/JobApp/Applicant/ApplicantDirectory.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package JobApp.Applicant;
+
+import Funding.PublicUser.PublicUserDirectory;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author SrushtiGhatage
+ */
+public class ApplicantDirectory {
+    
+    
+            
+}
