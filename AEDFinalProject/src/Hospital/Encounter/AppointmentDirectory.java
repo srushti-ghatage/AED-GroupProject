0cc05a488d69fd36e710324372b5b7e8f1ba8d39
srushti-ghatage <ghatage.s@northeastern.edu>
2023-04-08 10:51:18 -0400

    added appointment dir

--- AEDFinalProject/src/Hospital/Encounter/AppointmentDirectory.java
+++ AEDFinalProject/src/Hospital/Encounter/AppointmentDirectory.java
@@ -11,5 +11,39 @@ import java.util.ArrayList;
  * @author SrushtiGhatage
  */
 public class AppointmentDirectory {
+     private ArrayList<Appointment> hosAppList;
+
+    public AppointmentDirectory() {
+        hosAppList = new ArrayList<Appointment>();
+        
+    }
+
+    public ArrayList<Appointment> getHosAppList() {
+        return hosAppList;
+    }
+
+    public void setHosAppList(ArrayList<Appointment> hosAppList) {
+        this.hosAppList = hosAppList;
+    }
     
+    public Appointment addAppointment(){
+        Appointment newmanager = new Appointment();
+        hosAppList.add(newmanager);
+        return newmanager;
+    }
+    
+    public void deleteAppointment(Appointment m){
+        hosAppList.remove(m);
+    }
+    
+    public boolean uiqueAppId(String id) {
+
+        for (Appointment d : hosAppList) {
+
+            if (id.equals(d.getAppNo())) {
+                return false;
+            }
+        }
+            return true; 
+    } 
 }
