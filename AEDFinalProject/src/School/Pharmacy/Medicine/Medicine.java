42558da34fbf27bc4cd6239e593410a267c24217
MeghanaNandaKumar <nandakumar.me@northeastern.edu>
2023-04-09 02:17:33 -0400

    mores

--- AEDFinalProject/src/School/Pharmacy/Medicine/Medicine.java
+++ AEDFinalProject/src/School/Pharmacy/Medicine/Medicine.java
@@ -18,6 +18,67 @@ public class Medicine {
     private String desc;
     private String av;
 
-   
-   
+    public String getItemid() {
+        return itemid;
+    }
+
+    public void setItemid(String itemid) {
+        this.itemid = itemid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Integer getQty() {
+        return qty;
+    }
+
+    public void setQty(Integer qty) {
+        this.qty = qty;
+    }
+
+    public String getAv() {
+        return av;
+    }
+
+    public void setAv(String av) {
+        this.av = av;
+    }
+    
+    
+    
+    @Override
+    public String toString(){
+        String n = String.valueOf(itemid);
+        return n;
+    }
 }
