f638d1485c4772b6b21bdb3ca129350b87c40e3d
MeghanaNandaKumar <nandakumar.me@northeastern.edu>
2023-04-09 02:16:05 -0400

    new adds

---
+++ AEDFinalProject/src/School/Pharmacy/Medicine/Medicine.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pharmacy.Medicine;
+
+/**
+ *
+ * @author megha
+ */
+public class Medicine {
+    
+    private String itemid;
+    private String name;
+    private Integer cost;
+    private String date;
+    private Integer qty;
+    private String desc;
+    private String av;
+
+   
+   
+}
