2e1a107329a3bac7960a49f2cfe97e418b85b3ac
MeghanaNandaKumar <nandakumar.me@northeastern.edu>
2023-04-09 05:41:31 -0400

    more

--- AEDFinalProject/src/School/Student/StudentDirectory.java
+++ AEDFinalProject/src/School/Student/StudentDirectory.java
@@ -11,7 +11,41 @@ import java.util.ArrayList;
  * @author megha
  */
 public class StudentDirectory {
+    private ArrayList<Student> studentList;
+
+    public StudentDirectory() {
+        studentList = new ArrayList<Student>();
+        
+    }
+
+    public ArrayList<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(ArrayList<Student> studentList) {
+        this.studentList = studentList;
+    }
     
+    public Student addStudent(){
+        Student s = new Student();
+        studentList.add(s);
+        return s;
+    }
+    
+    public void deleteStudent(Student m){
+        studentList.remove(m);
+    }
+    
+    public boolean uiqueStudentId(String id) {
+
+        for (Student d : studentList) {
+
+            if (id.equals(d.getStudentId())) {
+                return false;
+            }
+        }
+            return true; 
+    } 
     
 
 }
