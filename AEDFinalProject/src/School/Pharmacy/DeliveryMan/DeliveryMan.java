33caa3b9b79e56ba312e35421fa9edf453400d0b
MeghanaNandaKumar <nandakumar.me@northeastern.edu>
2023-04-09 02:10:54 -0400

    added contents

--- AEDFinalProject/src/School/Pharmacy/DeliveryMan/DeliveryMan.java
+++ AEDFinalProject/src/School/Pharmacy/DeliveryMan/DeliveryMan.java
@@ -10,5 +10,91 @@ package Pharmacy.DeliveryMan;
  */
 public class DeliveryMan {
     
-   
+    private String deliverymanId;
+    private String name;
+    private String username;
+    private Integer age;
+    private String gender;
+    private String emailId;
+    private Long phno;
+    private String password;
+    private String role;
+
+    public String getDeliverymanId() {
+        return deliverymanId;
+    }
+
+    public void setDeliverymanId(String deliverymanId) {
+        this.deliverymanId = deliverymanId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public Long getPhno() {
+        return phno;
+    }
+
+    public void setPhno(Long phno) {
+        this.phno = phno;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+    @Override
+    public String toString(){
+        String n = String.valueOf(deliverymanId);
+        return n;
+    }
 }
