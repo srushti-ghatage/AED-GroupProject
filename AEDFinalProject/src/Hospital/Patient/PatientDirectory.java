ddb4f0283326b840e319a5c7617a1131b5075355
srushti-ghatage <ghatage.s@northeastern.edu>
2023-04-11 23:33:52 -0400

    find and unique patientid method added

--- AEDFinalProject/src/Hospital/Patient/PatientDirectory.java
+++ AEDFinalProject/src/Hospital/Patient/PatientDirectory.java
@@ -4,10 +4,53 @@
  */
 package Hospital.Patient;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author SrushtiGhatage
  */
 public class PatientDirectory {
+    private ArrayList<Patient> patientList;
+    
+    public PatientDirectory() {
+        patientList = new ArrayList<Patient>();   
+    }
+     public ArrayList<Patient> getPatientList() {
+        return patientList;
+    }
+
+    public void setPatientList(ArrayList<Patient> patientList) {
+        this.patientList = patientList;
+    }
+    public Patient addPatient(){
+        Patient newpatient = new Patient();
+        patientList.add(newpatient);
+        return newpatient;
+    }
+     public void deletePatient(Patient m){
+        patientList.remove(m);
+    }
+    
+    public Patient findPatient(String username, String password){
+        for(Patient n: patientList){
+            if(n.getUsername().equals(username) && n.getPassword().equals(password)){
+                return n;
+            }
+            
+        }
+        return null;
+    }
+    
+    public boolean uiquePatientId(String id) {
+
+        for (Patient d : patientList) {
+
+            if (id.equals(d.getPatientId())) {
+                return false;
+            }
+        }
+            return true; 
+    } 
     
 }
