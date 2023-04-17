07902fbbb66cc6e337f2817e8f79ffcfec49be8b
srushti-ghatage <ghatage.s@northeastern.edu>
2023-04-17 12:35:46 -0400

    funding manager changes

--- AEDFinalProject/src/Funding/FundManager/FundManager.java
+++ AEDFinalProject/src/Funding/FundManager/FundManager.java
@@ -9,5 +9,102 @@ package Funding.FundManager;
  * @author SrushtiGhatage
  */
 public class FundManager {
-   
+    private String fundManagerId;
+    private String name;
+    private String username;
+    private Integer age;
+    private String dob;
+    private String gender;
+    private String emailId;
+    private Long phno;
+    private String password;
+    private String role;
+
+    public String getFundManagerId() {
+        return fundManagerId;
+    }
+
+    public void setFundManagerId(String fundManagerId) {
+        this.fundManagerId = fundManagerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public Long getPhno() {
+        return phno;
+    }
+
+    public void setPhno(Long phno) {
+        this.phno = phno;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+    
+    @Override
+    public String toString(){
+        String n = String.valueOf(fundManagerId);
+        return n;
+    }
 }
