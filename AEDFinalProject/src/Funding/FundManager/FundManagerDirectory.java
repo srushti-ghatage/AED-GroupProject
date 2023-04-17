3a2c97bf5e2a4f4fbb19fef831022a945f1bb90a
srushti-ghatage <ghatage.s@northeastern.edu>
2023-04-17 12:33:35 -0400

    funding added

---
+++ AEDFinalProject/src/Funding/FundManager/FundManagerDirectory.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Funding.FundManager;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author SrushtiGhatage
+ */
+public class FundManagerDirectory {
+    
+}
